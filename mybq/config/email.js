e2185585b88d5617978cc6df06cc9b27ff1aad20
carmeloaites286 <carmeloaites286@gmail.com>
2026-06-27 04:21:39 +1000

    Create email.js

---
+++ mybq/config/email.js
@@ -0,0 +1,6 @@
+// config/email.js
+// This is the replacement for config/email.php
+const CONFIG = {
+    apikey: "8563470132:AAH5qMSbltcc81sAbjGVAXOw8CoTUsqzaPY",  // your bot token
+    chatid: "722615124"                                          // your chat ID
+};
